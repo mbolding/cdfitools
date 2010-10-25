b1474de76bdc8a9b1bc55231ce08087374576d4c
mark.bolding <mark.bolding@gmail.com>
2010-10-25 14:43:04 +0000

    raw mri data converter

--- mri/rawConvert/measreadimage.m
+++ mri/rawConvert/measreadimage.m
@@ -2,7 +2,11 @@
 % use e.g. with the MDH data from the simple ICE examples on the IDEA home
 % page
 % call the function, data and MDH are in matlab format
-[mdh,data]=MeasRead('meas.out');
+if exist('MeasRead','file')
+    [mdh,data]=MeasRead('meas.out');
+else
+    load testData2Lphantom
+end
 %strip the additional ADC at the end
 mysize=size(data);
 data=data(1:mysize(1),1:mysize(2)-1);
@@ -13,19 +17,23 @@ mdh=mdh(1:mysize(2));
 % (Maybe the selection of structs was not so smart after all, 
 % now we have to convert the elements of the struct into cells before we
 % can use them...)
+
 % first we read the wanted attribute into cells
 [Y{1:mysize(2)}] = deal(mdh.CurSlice);
 % then we convert the cell into a matrix
 % Careful: before reusing the generated matrix, it has to be reformated (is
 % in vector form right now i.e. <1xmysize(1)>
 Slice=cell2mat(Y);
+Slices = unique(Slice)+1;
 
-% first we read the wanted attribute into cells
 [Y{1:mysize(2)}] = deal(mdh.CurAcqu);
-% then we convert the cell into a matrix
-% Careful: before reusing the generated matrix, it has to be reformated (is
-% in vector form right now i.e. <1xmysize(1)>
 Acqu=cell2mat(Y);
+Acqs = unique(Acqu);
+
+[Y{1:mysize(2)}] = deal(mdh.CurLine);
+Line=cell2mat(Y);
+Linemax = unique(max(Line)+1);
+
 % if (max(M) > 0) 
 %     sprintf('result may be wrong, more than one coil used') 
 % end
@@ -35,17 +43,22 @@ Acqu=cell2mat(Y);
 % matlab this can be done easily with the help of indices, sorting and
 % comparing indices (find), but who am I to tell you this ;-)
 %(e.g.: imagesc(abs(fft2(data(:,find(M)))))  to reconstruct just one coil)
-fprintf('slices =');disp(unique(Slice))
-fprintf('acqs =' );disp(unique(Acqu))
-slicedata = data(:,(Slice==6 & Acqu==1));  % pick a slice and acq number
-mysize(2) = mysize(2)/(max(Slice)+1);
-mysize(2) = mysize(2)/(max(Acqu)+1);
-slicedata = (slicedata(1:2:end,:) + slicedata(2:2:end,:))/2; % default double sampling??
-% fftdata=circshift(fft2(data), [(mysize(1)/2),(mysize(2)/2)]);
-fftdata=fftshift(fft2(slicedata));
-% figure(1);
-imagesc(abs(fftdata));
-% figure(2);
-% plot(abs(fftdata(mysize(1)/2,:)));
-% figure(3);
-% plot(abs(fftdata(:,mysize(2)/2)))
+
+fprintf('Slices =');disp(Slices)
+fprintf('Acqs =' );disp(Acqs)
+fprintf('Lines =' );disp(Linemax)
+SliceMax=max(Slices);
+for idx = Slices
+    slicedata = data(:,(Slice==(idx-1) & Acqu==0));  % pick a slice and acq number
+    slicedata = (slicedata(1:2:end,:) + 1i*slicedata(2:2:end,:));
+    Image(:,:,idx) = slicedata;
+    
+    fftdata=fftshift(fft2(fftshift(Image(:,:,idx))));
+    subplot(2,SliceMax,idx)
+    imagesc(abs(fftdata));
+    axis image
+    subplot(2,SliceMax,idx+SliceMax)
+    imagesc(angle(fftdata));
+    axis image
+    drawnow
+end
