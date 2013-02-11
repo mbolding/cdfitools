7be9517c856fec153ab8107e04f892ed30bbf84e
zhxx89 <zhxx89@gmail.com>
2013-02-11 16:52:59 +0000

    design the block

---
+++ loadBlockDesignParameters.m
@@ -0,0 +1,33 @@
+
+  blocks = {'sacc'};
+  blockRepeats = 1;
+  blockDuration = 2255.7;
+
+% switch runNumber
+%     case 1
+%        % blocks, experiment design
+%        % blocks = {'fix','sacc','spem','vergtr','vergst'};
+%        blocks = {'sacc'};
+%         
+%      case 2
+%         % blocks, experiment design
+%         blocks = {'fix','vergtr','vergst','sacc','spem'};
+%      case 3
+%         % blocks, experiment design
+%         blocks = {'fix','sacc','vergtr','spem','vergst'};
+%      case 4
+%         % blocks, experiment design
+%         blocks = {'fix','vergst','vergtr','spem','sacc'};
+%      case 5
+%         % blocks, experiment design
+%         blocks = {'fix','spem','vergst','sacc','vergtr'};
+%      case 6
+%         % blocks, experiment design
+%         blocks = {'fix','vergtr','sacc','spem','vergst'};
+%     otherwise
+%         blocks = {'vergtr','vergst'};
+%         
+% end
+% runNumber = runNumber + 1;
+% blockDuration = 20;
+% blockRepeats = 1;
